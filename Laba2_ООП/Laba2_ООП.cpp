ff6f5a708e9a06bc2a6258efd32fd26f3a54207f
Khavanova Olga <khavanova@mail.ru>
2020-10-29 20:41:10 +0500

    Изменение 3 (Создание Class ColorPoint)

--- Laba2_ООП/Laba2_ООП.cpp
+++ Laba2_ООП/Laba2_ООП.cpp
@@ -3,13 +3,13 @@
 
 #include <iostream>
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
 class Point {
-protected:
-    int x, y;
 public:
+    int x, y;
 
     Point() {
         printf("'Пустая точка'");
@@ -55,6 +55,52 @@ void Point::reset() {
     printf("Метод reset класса Point запустился ");
     printf("(%d;%d)\n", x, y);
 }
+
+class ColorPoint : public Point {
+protected:
+    int Color;
+public:
+
+    ColorPoint() {
+        printf("'Пустая цветная точка'");
+        x = 0;
+        y = 0;
+        Color = 0;
+        printf("(%d;%d;%d)\n", x, y, Color);
+    }
+
+    ColorPoint(int x, int y, int Color) :Point(x, y) {
+        printf("'Цветная точка с координатами'");
+        this->x = x;
+        this->y = y;
+        this->Color = Color;
+        printf("Конструктор с параметрами класса СolorPoint запустился\n");
+        printf("(%d;%d;%d)\n", x, y, Color);
+    }
+
+    ColorPoint(int x, int y, int Color) :Point(x, y) {
+        printf("'Цветная точка с координатами'");
+        this->x = x;
+        this->y = y;
+        this->Color = Color;
+        printf("Конструктор с параметрами класса ColorPoint запустился\n");
+        printf("(%d;%d;%d)\n", x, y, Color);
+    }
+
+    ColorPoint(const ColorPoint& c) {
+        printf("'Цветная точка-копия'");
+        x = c.x;
+        y = c.y;
+        Color = c.Color;
+        printf("Конструктор копирования класса СolorPoint запустился\n");
+        printf("(%d;%d;%d)\n", x, y, Color);
+    }
+
+    ~ColorPoint() {
+        printf("Деструктор класса ColorPoint запустился\n");
+        printf("Удаление цветной точки с координатами (%d;%d;%d)\n", x, y, Color);
+    }
+};
 
 int main()
 {
